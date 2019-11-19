fce356b8bab173ea748b41fd2a122c6901788936
Kenneth Fukizi <48943835+KenOrderIn@users.noreply.github.com>
2019-11-19 15:20:22 +0200

    Chapter 04 in full

---
+++ Chapter04/TicTacToe/Controllers/UserRegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TicTacToe.Models;
+using TicTacToe.Services;
+
+namespace TicTacToe.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        private IUserService _userService;
+        public UserRegistrationController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userService.RegisterUser(userModel);
+                return Content($"User {userModel.FirstName} { userModel.LastName} has been registered sucessfully");
+            }
+            return View(userModel);
+        }
+    }
+}
---
+++ Chapter04/TicTacToe/Middlewares/CommunicationMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicTacToe.Services;
+
+namespace TicTacToe.Middlewares
+{
+    public class CommunicationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IUserService _userService;
+
+        public CommunicationMiddleware(RequestDelegate next,
+         IUserService userService)
+        {
+            _next = next;
+            _userService = userService;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next.Invoke(context);
+        }
+    }
+}
